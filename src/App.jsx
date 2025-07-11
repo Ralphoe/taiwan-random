4825dba3acdf7cb4f982dce45a6b0ecb219e9f22
Ralph <ralph341629@gmail.com>
2025-07-12 00:33:39 +0800

    Add basename to BrowserRouter for deployment
    Sets a `basename` in the `BrowserRouter` to support deployment under a subdirectory (`/taiwan-random`).
    
    Also updates import statements to use single quotes for consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StationPage from "./pages/StationPage/StationPage";
-import StationResult from "./pages/StationResult/StationResult";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import StationPage from './pages/StationPage/StationPage'
+import StationResult from './pages/StationResult/StationResult'
+import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/taiwan-random">
       <Routes>
         <Route path="/" element={<StationPage />} />
         <Route path="/result" element={<StationResult />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
